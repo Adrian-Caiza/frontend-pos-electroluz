bf1c7b6f6e7d61ffead2f29a2294caa7f8132837
Adrian-Caiza <adrian.caiza@epn.edu.ec>
2026-06-22 23:20:45 -0500

    fix: Arreglado bug de doble ojito en contraseña.

--- src/shared/components/ui/password-input.tsx
+++ src/shared/components/ui/password-input.tsx
@@ -23,6 +23,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
           type={showPassword ? "text" : "password"}
           className={cn(
             "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+            "[&::-ms-reveal]:hidden [&::-ms-clear]:hidden",
             Icon && "pl-10",
             className
           )}
